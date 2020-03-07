8c7d96dad24cf96d2f73b864228635d8b20f55c7
Alexander Hartson <alexander-hartson@live.com>
2020-03-07 14:12:46 -0500

    setting up basic page

--- ng_BandManagement/src/app/app.module.ts
+++ ng_BandManagement/src/app/app.module.ts
@@ -2,14 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { UserDetailComponent } from './user-detail/user-detail.component';
-import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UserDetailComponent,
-    UserComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
