607381bed4b7de97da705ca10cd1ead17449b681
Inioluwa OluAlakija <inioluwa4@gmail.com>
2020-03-08 11:32:30 -0400

    Bootstrap & Login

--- java_BMS/src/main/java/com/revature/controller/RequestMapper.java
+++ java_BMS/src/main/java/com/revature/controller/RequestMapper.java
@@ -57,9 +57,7 @@ public class RequestMapper {
 		delegateMap = new HashMap<String, FrontControllerDelegate>();
 		delegateMap.put("login", new LoginDelegate());
 		
-		delegateMap.put("home", (req, resp) -> {
-			req.getRequestDispatcher("/static/books.html").forward(req, resp);
-		});
+
 
 
 	}
