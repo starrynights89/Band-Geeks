9609678bbf1b4717ffca81c9bc50719038063045
Inioluwa OluAlakija <inioluwa4@gmail.com>
2020-03-12 13:52:53 -0400

    Assignments features otw

---
+++ ng_BandManagement/src/app/classes/assignment-type.spec.ts
@@ -0,0 +1,7 @@
+import { AssignmentType } from './assignment-type';
+
+describe('AssignmentType', () => {
+  it('should create an instance', () => {
+    expect(new AssignmentType()).toBeTruthy();
+  });
+});
