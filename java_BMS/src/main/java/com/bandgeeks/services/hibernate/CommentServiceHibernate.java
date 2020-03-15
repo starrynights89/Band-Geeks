f41460ab51fc931638fc4bde518a808a12685343
Alexander Hartson <alexander-hartson@live.com>
2020-03-15 18:27:18 -0400

    adding comments backend

--- java_BMS/src/main/java/com/bandgeeks/services/hibernate/CommentServiceHibernate.java
+++ java_BMS/src/main/java/com/bandgeeks/services/hibernate/CommentServiceHibernate.java
@@ -2,21 +2,35 @@ package com.bandgeeks.services.hibernate;
 
 import java.util.Set;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.bandgeeks.beans.Comment;
 import com.bandgeeks.data.hibernate.CommentDAO;
 
+@Service
 public class CommentServiceHibernate implements CommentDAO {
 
+	private Logger log = Logger.getLogger(RequestServiceHibernate.class);
+	@Autowired
+	private CommentDAO cd;
+	
 	@Override
 	public Integer addComment(Comment comment) {
-		// TODO Auto-generated method stub
-		return null;
+		log.trace("attempting to add comment");
+		cd.addComment(comment);
+		return comment.getCommentsId();
+		
 	}
 
 	@Override
 	public Comment getComment(Comment comment) {
-		// TODO Auto-generated method stub
-		return null;
+		log.trace("attempting to get comment");
+		if (comment == null) {
+			return null;
+		} else cd.getComment(comment);
+		return comment;
 	}
 
 	@Override
